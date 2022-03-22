7cd6983415c56e4924ffdb72d340f585d64f58a6
Jonas <jonascorpes865@gmail.com>
2022-03-21 22:05:36 -0300

    third commit

---
+++ Aula7/Program.cs
@@ -0,0 +1,74 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Aula_7 
+{ 
+
+   internal class Program 
+   { 
+    
+      static void Main(string[] args) 
+      { 
+          Console.WriteLine("Cestinha!");
+          string[] cestadefrutas = { "maçã", "banana", "uva", "manga" };
+          
+          for (int i = 0; i < cestadefrutas.Length; i++) 
+          {
+                Console.WriteLine(cestadefrutas[i]);
+          }
+
+            Console.WriteLine("Digite um dia da semana!");
+            int dia = int.Parse(Console.ReadLine());
+            switch(dia) 
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Dom");
+                    }
+                    break;
+
+                case 2: 
+                    {
+                        Console.WriteLine("Seg");
+                    }
+                    break;
+
+                case 3:
+                    {
+                        Console.WriteLine("Ter");
+                    }
+                    break;
+
+                case 4: 
+                    {
+                        Console.WriteLine("Qua");
+                    }
+                    break;
+
+                case 5:
+                    {
+                        Console.WriteLine("Qui");
+                    }
+                    break;
+
+                case 6:
+                    {
+                        Console.WriteLine("Sex");
+                    }
+                    break;
+
+                case 7:
+                    {
+                        Console.WriteLine("Sab");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("nenhum dia = (");
+                    break;
+            }
+      }
+   }
+}
