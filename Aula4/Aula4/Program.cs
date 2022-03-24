c08fa8e1ebaac33fb5fd7f247d53efb7fc9bef5b
Jonas <jonascorpes865@gmail.com>
2022-03-24 10:17:46 -0300

    fourth commit

---
+++ Aula4/Aula4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula4;
+
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("digite um número");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+            Console.WriteLine("número par");
+        else
+            Console.WriteLine("número impar");
+
+        Console.ReadKey();
+    
+    
+    }
+    
+}
+
